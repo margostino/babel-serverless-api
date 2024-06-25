c299131dc5fe9db6eb5e22e15bacc0df92053cfa
Martín D'Agostino <martin.dagostino@klarna.com>
2024-06-25 22:37:20 +0200

    eslint and prettier stuff

---
+++ internal/utils/randomMessages.ts
@@ -0,0 +1,10 @@
+
+const messages = [
+  'Hello, world!',
+  'How are you today?',
+    'What a lovely day!',
+    'Good to see you!',
+    'How are you doing?',
+]
+
+//const getRandomMessage = () => {
