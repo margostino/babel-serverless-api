08c5d77d97c1eabd1642162217da8d27b743b8c6
Martín D'Agostino <martin.dagostino@klarna.com>
2024-07-03 22:27:50 +0200

    wip

--- src/memory/getMemoriesByKeys.ts
+++ src/memory/getMemoriesByKeys.ts
@@ -1,12 +1,21 @@
 import { getData } from '../github'
+import { logger } from '../logger'
 
 export const getMemoriesByKeys = async (keys: string[]) => {
   const memoriesAssets = await Promise.all(
     keys.map(async (key: string) => {
-      const data = await getData(key)
-      return {
-        path: key,
-        content: data,
+      try {
+        const data = await getData(key)
+        return {
+          path: key,
+          content: data,
+        }
+      } catch (error) {
+        logger.error(`Failed to get memory by key: ${key}`)
+        return {
+          path: key,
+          content: null,
+        }
       }
     })
   )
